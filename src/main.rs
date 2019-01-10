63f33852333f1386f34da24d9e632be0a06456ea
Andy Lowry <andy.lowry@moonpig.com>
2019-01-10 00:11:22 +0000

    mut inc examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#![deny(warnings)]
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 23;
+    let mut y = &mut x;
+
+    inc(&mut y);
+    println!("{}", y);
+
+    inc(&mut x);
+    println!("{}", x);
+
+    let z = &x;
+    println!("{}", z);
+}
+
+fn inc(x: &mut i32) {
+    *x += 1;
 }
